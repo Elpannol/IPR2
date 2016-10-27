f71e5b7fb06186e5d6a12c182f9970aa7c5dfc79
Gijs San <gijs.brian.joost@hotmail.com>
2016-10-27 15:35:08 +0200

    no databases, just clients

---
+++ IPR2/IPR2/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPR2
+{
+    class Client
+    {
+        public TcpClient _Client { get; set; }
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public bool IsDoctor { get; set; }
+        public Client(TcpClient client, string name, int Id, bool isDoctor)
+        {
+            _Client = client;
+            Name = name;
+            ID = Id;
+            IsDoctor = isDoctor;
+        }
+    }
+}
