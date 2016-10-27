e1f85ee2b15848fcd7d603ce2a48525f632ce43b
Gijs San <gijs.brian.joost@hotmail.com>
2016-10-27 15:14:09 +0200

    Begin

---
+++ IPR2/IPR2/ClientHandler.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPR2
+{
+    class ClientHandler
+    {
+        private TcpClient _client;
+
+        public ClientHandler(TcpClient client)
+        {
+            _client = client;
+        }
+
+        public void HandleClientThread()
+        {
+            do
+            {
+                dynamic message = JsonConvert.DeserializeObject(ReadMessage(_client));
+                switch ((string)message.id)
+                {
+                    case "Protocol/eenheden":
+                        break;
+                    default:
+                        Console.WriteLine("You're not suppose to be here");
+                        break;
+                }
+            }
+            while (_client.Connected);
+        }
+
+        //TODO: both read and send message need to work with Json
+        public static string ReadMessage(TcpClient client)
+        {
+
+            byte[] buffer = new byte[128];
+            int totalRead = 0;
+
+            //read bytes until stream indicates there are no more
+            do
+            {
+                int read = client.GetStream().Read(buffer, totalRead, buffer.Length - totalRead);
+                totalRead += read;
+                Console.WriteLine("ReadMessage: " + read);
+            } while (client.GetStream().DataAvailable);
+            string message = Encoding.Unicode.GetString(buffer, 0, totalRead);
+            Console.WriteLine(message);
+            return message;
+        }
+
+        public static void SendMessage(TcpClient client, dynamic message)
+        {
+            //make sure the other end decodes with the same format!
+            message = JsonConvert.SerializeObject(message);
+            byte[] bytes = Encoding.Unicode.GetBytes(message);
+            client.GetStream().Write(bytes, 0, bytes.Length);
+            client.GetStream().Flush();
+        }
+    }
+}
---
+++ IPR2/IPR2/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IPR2
+{
+    class Server
+    {
+        private TcpListener _listener;
+        private List<ClientHandler> _handlers;
+        private IPAddress _currentId;
+
+        public Server()
+        {
+            IPAddress localIP = GetLocalIpAddress();
+            _handlers = new List<ClientHandler>();
+            string LogName = "SessionLog/" + DateTime.Today + "/" + DateTime.Now + "/ID=" + _handlers.Count;
+
+            bool IpOk = IPAddress.TryParse(localIP.ToString(), out _currentId);
+            if (!IpOk)
+            {
+                Console.WriteLine("Couldn't parse the ip address. Exiting code.");
+                Environment.Exit(1);
+            }
+
+            TcpListener listener = new TcpListener(_currentId, 1337);
+            listener.Start();
+
+            //making client handlers and adding them to the list
+            while (true)
+            {
+                ClientHandler handler = new ClientHandler(CheckForClients(_listener));
+                Thread thread = new Thread(handler.HandleClientThread);
+                thread.Start();
+                _handlers.Add(handler);
+            }
+        }
+
+        private TcpClient CheckForClients(TcpListener listner)
+        {
+            Console.WriteLine("Waiting for clients");
+            TcpClient client = listner.AcceptTcpClient();
+            Console.WriteLine("Client connected!!");
+
+            return client;
+        }
+
+        public static IPAddress GetLocalIpAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip;
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
