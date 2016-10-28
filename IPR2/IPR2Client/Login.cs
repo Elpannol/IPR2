40fc933c48366385a394655dea4c6be54531e524
Cas Koopmans <cjwkoopm@avans.nl>
2016-10-28 13:33:41 +0200

    New Project

---
+++ IPR2/IPR2Client/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPR2Client
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            wrongLabel.Visible = false;
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(tryLogin(gebruikersNaam.Text, wachtwoord.Text))
+            {
+
+            }
+            else
+            wrongLabel.Visible = true;
+        }
+
+        private bool tryLogin(string gebruikersnaam, string wachtwoord)
+        {
+            return false;
+        }
+    }
+}
