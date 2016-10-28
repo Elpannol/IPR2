b998c6ec5fd0bfec2226310b4d53bd63c277ec35
Cas Koopmans <cjwkoopm@avans.nl>
2016-10-28 14:45:31 +0200

    tried simulator can you make it word? xD

---
+++ IPR2/IPR2Client/Simulation/Measurement.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPR2Client.Simulation
+{
+    class Measurement : IComparable<Measurement>
+    {
+        public int Weerstand { get; set; }
+        public int Hartslag { get; set; }
+        public int Rondes { get; set; }
+        public SimpleTime Time { get; set; }
+
+        public Measurement(int weerstand, int hartslag, int rondes, int time1, int time2)
+        {
+            Weerstand = weerstand;
+            Hartslag = hartslag;
+            Rondes = rondes;
+            Time = new SimpleTime(time1, time2);
+        }
+
+        public int CompareTo(Measurement other)
+        {
+            if (Time == other.Time)
+                return 0;
+            if (Time > other.Time)
+                return 1;
+            if (Time < other.Time)
+                return -1;
+            return 0;
+        }
+
+        private dynamic GetMessageToSend()
+        {
+            dynamic toSend = new
+            {
+                id = "measurement/add",
+                data = new
+                {
+                    weerstand = Weerstand,
+                    hartslag = Hartslag,
+                    rondes = Rondes,
+                    time = Time
+                }
+            };
+            return toSend;
+        }
+    }
+
+    public struct SimpleTime
+    {
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is SimpleTime && Equals((SimpleTime)obj);
+        }
+
+        public bool Equals(SimpleTime other)
+        {
+            return (Minutes == other.Minutes) && (Seconds == other.Seconds);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Minutes * 397) ^ Seconds;
+            }
+        }
+
+        public readonly int Minutes;
+        public readonly int Seconds;
+
+        public SimpleTime(int min, int sec)
+        {
+            Minutes = min;
+            Seconds = sec;
+        }
+
+        public override string ToString()
+        {
+            return $"{Minutes:00}:{Seconds:00}";
+        }
+
+        public static bool operator >(SimpleTime first, SimpleTime second)
+        {
+            if (first.Minutes > second.Minutes)
+                return true;
+            if (first.Minutes == second.Minutes)
+                return first.Seconds > second.Seconds ? true : false;
+            return false;
+        }
+
+        public static bool operator <(SimpleTime first, SimpleTime second)
+        {
+            if (first.Minutes < second.Minutes)
+                return true;
+            if (first.Minutes == second.Minutes)
+                return first.Seconds < second.Seconds ? true : false;
+            return false;
+        }
+
+        public static bool operator ==(SimpleTime first, SimpleTime second)
+        {
+            return (first.Minutes == second.Minutes) && (first.Seconds == second.Seconds);
+        }
+
+        public static bool operator !=(SimpleTime first, SimpleTime second)
+        {
+            return (first.Minutes != second.Minutes) || (first.Seconds != second.Seconds);
+        }
+    }
+}
--- IPR2/IPR2Client/Simulation/Simulator.cs
+++ IPR2/IPR2Client/Simulation/Simulator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IPR2Client.Simulation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,15 +13,27 @@ namespace IPR2Client.Forms
 {
     public partial class Simulator : Form
     {
+        private readonly Measurement Measurement;
+        private int _time;
+
         public Simulator()
         {
+            _time = 0;
+            SimpleTime temp = new SimpleTime(_time / 60, _time % 60);
+            Measurement = new Measurement(20, 100, 50, temp.Minutes, temp.Seconds);
             InitializeComponent();
             FormClosing += Simulator_FormClosing;
+            UpdateData();
         }
 
         private void Simulator_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void UpdateData()
+        {
+
         }
     }
 }
